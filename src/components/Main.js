c86dd5c494bb47ce93873de2e1b70cdfbd849536
DavideDaniel <davidedaniel@gmail.com>
2016-02-18 03:20:40 -0800

    additional routes pages added from mock

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,13 +7,30 @@ import {Link} from 'react-router';
 
 const Main = ({children}) => {
 	return (
-      <div className="main-container">
+      <div>
         <nav className="navbar navbar-default" role="navigation">
-          <div className="col-sm-7 col-sm-offset-2">
-						<Menu items={[
-        			<Link to = "/" activeClass="active"> Home </Link>,
-        			<Link to = "about/" activeClass="active"> About </Link>
+					<div className="container-fluid">
+					<div className="navbar-header">
+      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navLinks" aria-expanded="false">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+			<Link to = "/" className="navbar-brand"> Behind <span>the</span> Curtain </Link>
+    </div>
+
+			<div className="collapse navbar-collapse id="navLinks"">
+						<Menu className="navbar-left"items={[
+        			<Link to = "search" activeStyle={{text-decoration:'underline'}}> Search </Link>,
+        			<Link to = "oregon" activeStyle={{text-decoration:'underline'}}> Oregon </Link>,
+							<Link to = "campaigns" activeStyle={{text-decoration:'underline'}}> Campaigns </Link>,
+							<Link to = "donors" activeStyle={{text-decoration:'underline'}}> Donors </Link>,
+							<Link to = "candidates" activeStyle={{text-decoration:'underline'}}> Candidates </Link>,
+        			<Link to = "about" activeStyle={{text-decoration:'underline'}}> About </Link>,
+							<Link to = "faq" activeStyle={{text-decoration:'underline'}}> FAQ </Link>
       			]}/>
+          </div>
           </div>
         </nav>
         <div className="container">
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,15 +1,24 @@
-// home, search, OR, About, FAQ
-
 import React from 'react';
 import Main from '../components/Main';
-import Home from '../components/Home';
-import About from '../components/About';
-import {Router, Route, IndexRoute, NotFoundRoute, DefaultRoute, Link, hashHistory} from 'react-router';
-
+import HomePage from '../components/HomePage';
+import AboutPage from '../components/AboutPage';
+import SearchPage from '../components/SearchPage';
+import DonorsPage from '../components/DonorsPage';
+import CampaignsPage from '../components/CampaignsPage';
+import CandidatesPage from '../components/CandidatesPage';
+import OregonPage from '../components/OregonPage';
+import FaqPage from '../components/FaqPage';
+import {Route, IndexRoute} from 'react-router';
 
 export default (
   <Route path="/" component={Main}>
-    <Route path="about/" component={About} />
-    <IndexRoute component={Home} />
+    <Route path="search" component={SearchPage} />
+    <Route path="oregon" component={OregonPage} />
+    <Route path="campaigns" component={CampaignsPage} />
+    <Route path="donors" component={DonorsPage} />
+    <Route path="candidates" component={CandidatesPage} />
+    <Route path="about" component={AboutPage} />
+    <Route path="faq" component={FaqPage} />
+    <IndexRoute component={HomePage} />
   </Route>
 );
